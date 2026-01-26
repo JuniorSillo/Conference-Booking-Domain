4b3ecc562e3e78159f63093790608d904b3572be
JuniorSillo <123242158+JuniorSillo@users.noreply.github.com>
2026-01-26 22:40:01 +0200

    Updating Program.cs File (Implementing a conference booking console demo for testing)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConferenceBooking.Domain.Models;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            var room = ConferenceRoom.Create(
+                roomid: "BRD-101",
+                roomname: "Executive Boardroom",
+                capacity: 12,
+                location: "Floor 4, Bitcube HQ",
+                amenities: RoomAmenity.Projector | RoomAmenity.VideoConference | RoomAmenity.NaturalLight
+            );
+
+            Console.WriteLine("Created room: " + room);
+
+            var booking = Booking.Create(
+                bookingid: "BOOK-20260126-001",
+                roomId: room.RoomID,
+                bookedByUserId: "USR-juniorsillo123",
+                startTime: new DateTime(2026, 2, 1, 10, 0, 0, DateTimeKind.Utc),
+                endTime: new DateTime(2026, 2, 1, 11, 30, 0, DateTimeKind.Utc),
+                purpose: "Q1 Strategy Meeting"
+            );
+
+            Console.WriteLine("Created booking: " + booking);
+
+            // Demonstrate status change
+            booking.ChangeStatus(BookingStatus.Approved);
+            Console.WriteLine("After approval: " + booking);
+
+          
+            // Overlap checking example(COnflict detection)
+            var overlaps = booking.OverlapsWith(
+                new DateTime(2026, 2, 1, 11, 0, 0, DateTimeKind.Utc),
+                new DateTime(2026, 2, 1, 12, 0, 0, DateTimeKind.Utc));
+
+            Console.WriteLine($"Overlaps with 11:00-12:00? {overlaps}"); // true
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+    }
+}
