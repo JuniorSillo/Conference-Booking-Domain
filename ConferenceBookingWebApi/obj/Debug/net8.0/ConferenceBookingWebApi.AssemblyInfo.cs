39cdae90b9ab6623841439d1b74c521d4da3cfc7
JuniorSillo <123242158+JuniorSillo@users.noreply.github.com>
2026-02-28 16:31:42 +0200

    Update All files

--- ConferenceBookingWebApi/obj/Debug/net8.0/ConferenceBookingWebApi.AssemblyInfo.cs
+++ ConferenceBookingWebApi/obj/Debug/net8.0/ConferenceBookingWebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConferenceBookingWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a423b7d6323845fae89554fa94855a66b392acda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8367f41b9129365ef4359fc93dbe7d9fe5752456")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConferenceBookingWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConferenceBookingWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
